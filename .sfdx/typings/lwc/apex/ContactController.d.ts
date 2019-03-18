297bb1fe0839308dfb78318405132a13d350a053
Dmitry Loktevich <loktevich@gmail.com>
2019-03-18 19:30:20 +0300

    Remove ignored files

--- .sfdx/typings/lwc/apex/ContactController.d.ts
+++
@@ -1,3 +0,0 @@
-declare module "@salesforce/apex/ContactController.searchContacts" {
-  export default function searchContacts(param: {searchName: any, sortedBy: any, sortDirection: any, pageNumber: any, pageSize: any}): Promise<any>;
-}
